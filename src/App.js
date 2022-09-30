09a72abd866f52acdfdfd3264c2aca00e5b673e8
Arnaud Cavalletto <arnaudcavalletto@yahoo.com>
2022-09-30 14:35:47 -0600

    removed redundant show page for gallery as link is now going to separate site.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Landing from './pages/Landing'
 import Pricing from './pages/Pricing'
 import GalleryIndex from './pages/GalleryIndex'
-import Gallery1 from './pages/Gallery1'
 import Contact from './pages/Contact'
 import Testimonials from './pages/Testimonials'
 import { Route } from 'react-router'
@@ -22,9 +21,6 @@ function App() {
       </Route>
       <Route path='/collections'>
         <GalleryIndex />
-      </Route>
-      <Route path='/collection1'>
-        <Gallery1 />
       </Route>
       <Route path='/testimonials'>
         <Testimonials />
